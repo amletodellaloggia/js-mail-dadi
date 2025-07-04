85cc99f686098d0a3902a85c121c86ac76061c94
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-04 14:23:34 +0200

    [add] message variable

--- js/script.js
+++ js/script.js
@@ -9,4 +9,11 @@ const invitedEmails = [
 ];
 const userEmail = prompt("Inserisci la tua email:");
 
+// Effettuo un controllo che vede se la mail inserita è presente nella lista email
+// Utilizzo una variabile booleana inizializzata a 'false' che mi permette di far partire il messaggio per l'utente, e che cambierà valore e quindi messaggio a seconda del risultato
+
+let isUserInvited = false;
+
+
+
 
