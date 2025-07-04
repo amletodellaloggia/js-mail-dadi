8729247dba63e3de568ad60b459dafd40b547abd
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-04 14:15:16 +0200

    [add] variables

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// SVOLGIMENTO
+// ESERCIZIO 1 - MAIL
+
+// Creo variabile lista mail invitate e variabile mail che inserirà l'utente
+const invitedEmails = [
+  `topolino@email.com`,
+  `pippo@email.com`,
+  `paperino@email.com`,
+];
+const userEmail = prompt("Inserisci la tua email:");
+
+
