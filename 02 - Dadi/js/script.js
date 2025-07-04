ff0633dc3048c957d9c1134aeb52e90bcc77e6db
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-04 15:28:34 +0200

    [add] variables (initialized)

---
+++ 02 - Dadi/js/script.js
@@ -0,0 +1,10 @@
+// SVOLGIMENTO
+// ESERCIZIO 2 - DADI
+
+// Dichiaro due variabili che assumeranno il valore numerico (da 1 a 6) generato da 'math.random', ma utilizzerò math.floor per mostrare solo gli interi e +1 per evitare lo 0
+
+// Le variabili sono anche inizializzate perché entreranno subito in un controllo 'if'
+
+let	pcNumber = Math.floor(Math.random() *6 +1);
+let playerNumber = Math.floor(Math.random() *6) +1;
+
